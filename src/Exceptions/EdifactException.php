7e3b5ff1a51cdc566d008c4fe24589f5c2d6d104
Alexander Hermann <alex.hermann@icloud.com>
2016-10-05 01:36:42 +0200

    added description

--- src/Exceptions/EdifactException.php
+++ src/Exceptions/EdifactException.php
@@ -4,13 +4,28 @@ namespace Proengeno\Edifact\Exceptions;
 
 use RuntimeException;
 
+/**
+ * Edifact base exception with some static functions.
+ */
 class EdifactException extends RuntimeException
 {
+	/**
+	 * Static function for unknown segments.
+	 *  
+	 * @param string $segment an EDIFACT segment
+	 * @return \Proengeno\Edifact\Exceptions\EdifactException 
+	 */
     public static function segmentUnknown($segment)
     {
         return new static("Segment not registered: '$segment'");
     }
 
+    /**
+     * Static function for unknown messages.
+     * 
+     * @param string $message an EDIFACT message
+     * @return \Proengeno\Edifact\Exceptions\EdifactException
+     */
     public static function messageUnknown($message)
     {
         return new static("Message not registered: '$message'");
--- src/Exceptions/SegValidationException.php
+++ src/Exceptions/SegValidationException.php
@@ -2,11 +2,31 @@
 
 namespace Proengeno\Edifact\Exceptions;
 
+use Proengeno\Edifact\Message\Message;
+
+/**
+ * An exception for segment validations.
+ */
 class SegValidationException extends EdifactException
 {
+	/**
+	 * Key
+	 * @var string
+	 */
     protected $key;
+    /**
+     * Value
+     * @var string
+     */
     protected $value;
 
+    /**
+     * Constructor
+     * @param string $key a key
+     * @param string $value a value
+     * @param string $message a message
+     * @param string $code some code
+     */
     public function __construct($key, $value, $message, $code)
     {
         $this->setKey($key);
@@ -14,6 +34,14 @@ class SegValidationException extends EdifactException
         parent::__construct($message, $code);
     }
     
+    /**
+     * A static function for key value messages
+     * @param string $key a key
+     * @param string $value a value
+     * @param string $message a message
+     * @param string $code [optional] some code
+     * @return \Proengeno\Edifact\Exceptions\SegValidationException
+     */
     public static function forKeyValue($key, $value, $message, $code = 0)
     {
         $message = $key . ' (' . $value . ') : ' . $message;
@@ -21,6 +49,13 @@ class SegValidationException extends EdifactException
         return new static($key, $value, $message, $code);
     }
 
+    /**
+     * Static function for key messages
+     * @param string $key a key
+     * @param string $message a message
+     * @param string $code [optional] some code
+     * @return \Proengeno\Edifact\Exceptions\SegValidationException
+     */
     public static function forKey($key, $message, $code = 0)
     {
         $message = $key . ': ' . $message;
@@ -28,6 +63,13 @@ class SegValidationException extends EdifactException
         return new static($key, null, $message, $code);
     }
 
+    /**
+     * Static function for value messages
+     * @param string $value a value
+     * @param string $message a message
+     * @param string $code [optional] some code
+     * @return \Proengeno\Edifact\Exceptions\SegValidationException
+     */
     public static function forValue($value, $message, $code = 0)
     {
         $message = $value . ': ' . $message;
@@ -35,21 +77,39 @@ class SegValidationException extends EdifactException
         return new static(null, $value, $message, $code);
     }
 
+    /**
+     * Gets the current key
+     * @return string the current key
+     */
     public function getKey()
     {
         return $this->key;
     }
 
+    /**
+     * Gets the current value
+     * @return string the current value
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * Sets the current key
+     * @param string $key the key to set
+     * @return string the current (new) key
+     */
     private function setKey($key)
     {
         return $this->key = $key;
     }
 
+    /**
+     * Sets the current value
+     * @param string $value the value to set
+     * @return string the current (new) value
+     */
     private function setValue($value)
     {
         return $this->value = $value;
